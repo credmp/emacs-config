d8e2d0aec690e019c2dfe85d9fa716894fd4823e
Arjen Wiersma <arjen@wiersma.org>
2016-08-24 21:31:51 +0200

    Add which-key, cleanup init.el

--- init.el
+++ init.el
@@ -2,31 +2,10 @@
 ;;
 ;; (c) 2000 - 2016 Arjen Wiersma
 ;;
-
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (require 'org)
 (require 'ob-tangle)
 
 (setq init-dir (file-name-directory (or load-file-name (buffer-file-name))))
 (org-babel-load-file (expand-file-name "loader.org" init-dir))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (zencoding-mode less-css-mode web-mode clj-refactor cider restclient magit company yasnippet rainbow-delimiters highlight-parentheses paredit-everywhere paredit wakatime-mode hydra s htmlize markdown-mode request perspective arjen-grey-theme ace-jump-mode ace-window counsel bm use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
